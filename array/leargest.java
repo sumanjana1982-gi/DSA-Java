4b02dcbec3fc5ad95401e09c49b09618a334b798
sumanjana1982 <jalpana.jana1982@gmail.com>
2026-06-27 11:41:27 +0530

    adding code

---
+++ array/leargest.java
@@ -0,0 +1,16 @@
+public class leargest{
+    public static int LA(int a[]){
+        int larg=a[0];
+        for(int i=1;i<a.length;i++){
+            if(a[i]>larg){
+            larg=a[i];
+            }
+        }
+        return larg;
+    }
+    public static void main(String arg[]){
+        int a[]={1,3,4,5,6,7,89,9,};
+       int result= leargest.LA(a[]);
+       System.out.print("largest in the given array is "+result);
+    }
+}
